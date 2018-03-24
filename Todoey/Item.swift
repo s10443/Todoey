01f9e2bde40065d21298f93f55ef7ca9909dece0
吳奇憲 <chihsien@wuqixiande-macbook-air.local>
2018-03-24 19:15:00 +0800

    Convert to MVC design pattern.

---
+++ Todoey/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by 吳奇憲 on 2018/3/24.
+//  Copyright © 2018年 吳奇憲. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
